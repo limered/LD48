38311b1961f8c56a90c10409e15a76518b8170c7
Emil Wasilewski <emil.wasilewski@brickmakers.de>
2021-04-24 17:02:55 +0200

    copy movement systems from LD45

---
+++ PutYourHandsUpInTheAir/Assets/Systems/Player/PlayerComponent.cs
@@ -0,0 +1,10 @@
+﻿using SystemBase;
+using UniRx;
+
+namespace Systems.Player
+{
+    public class PlayerComponent : GameComponent
+    {
+        public BoolReactiveProperty IsMoving;
+    }
+}
---
+++ PutYourHandsUpInTheAir/Assets/Systems/Player/PlayerSystem.cs
@@ -0,0 +1,78 @@
+﻿using SystemBase;
+using Systems.Movement;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+using Utils.Unity;
+using Object = UnityEngine.Object;
+
+namespace Systems.Player
+{
+    [GameSystem(typeof(MovementSystem))]
+    public class PlayerSystem : GameSystem<PlayerComponent, MovementComponent, PlayerSpawnerComponent>
+    {
+        private GameObject _currentPlayer;
+
+        public override void Register(PlayerComponent component)
+        {
+            if (_currentPlayer)
+            {
+                Object.Destroy(component.gameObject);
+            }
+            _currentPlayer = component.gameObject;
+
+            component.GetComponent<MovementComponent>().Direction
+                .Subscribe(dir => component.IsMoving.Value = dir.magnitude > 0);
+
+            component.IsMoving.Subscribe(obj => StartMovingState(component)).AddTo(component);
+
+        }
+
+        private void StartMovingState(PlayerComponent component)
+        {
+            component.GetComponentInChildren<Animator>().SetFloat("Speed", component.IsMoving.Value ? 7f : 0f);
+        }
+
+        public override void Register(MovementComponent component)
+        {
+            component.FixedUpdateAsObservable()
+                .Select(_ => component)
+                .Where(movementComponent => movementComponent.GetComponent<PlayerComponent>())
+                .Subscribe(ControlPlayer)
+                .AddTo(component);
+        }
+
+        private static void ControlPlayer(MovementComponent comp)
+        {
+            var movementDirection = Vector2.zero;
+            if (KeyCode.UpArrow.IsPressed())
+            {
+                movementDirection.y += 1;
+            }
+
+            if (KeyCode.DownArrow.IsPressed())
+            {
+                movementDirection.y += -1;
+            }
+
+            if (KeyCode.LeftArrow.IsPressed())
+            {
+                movementDirection.x += -1;
+            }
+
+            if (KeyCode.RightArrow.IsPressed())
+            {
+                movementDirection.x += 1;
+            }
+
+            comp.Direction.Value = movementDirection;
+        }
+
+        public override void Register(PlayerSpawnerComponent component)
+        {
+            MessageBroker.Default.Receive<ActPlayerRespawn>()
+                .Subscribe(respawn =>
+                    Object.Instantiate(component.PlayerPrefab, component.transform.position, Quaternion.identity));
+        }
+    }
+}
