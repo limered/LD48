c422d77d1d1802eae24b0454462985e0f569224c
Emil Wasilewski <emil.wasilewski@brickmakers.de>
2021-04-25 00:10:46 +0200

    find a random tourist without distraction

---
+++ PutYourHandsUpInTheAir/Assets/Systems/Distractions/RandomTouristFinder.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Systems.Tourist;
+using Systems.Tourist.States;
+using UnityEngine;
+using Utils.DotNet;
+
+namespace Systems.Distractions
+{
+    public class RandomTouristFinder
+    {
+        public TouristBrainComponent FindRandomTouristWithoutDistraction()
+        {
+            List<TouristBrainComponent> touristBrains = GameObject.FindGameObjectsWithTag("tourist")
+                .Select(t => t.GetComponent<TouristBrainComponent>())
+                .Where(brain => brain.States.CurrentState.Value is Idle)
+                .ToList();
+
+            return GerRandomTouristWithoutDistraction(touristBrains);
+        }
+
+        private TouristBrainComponent GerRandomTouristWithoutDistraction(List<TouristBrainComponent> touristBrains)
+        {
+            return touristBrains
+                .Randomize()
+                .FirstOrDefault(touristBrain => touristBrain.GetComponent<BaseDistractionComponent>() == null);
+        }
+    }
+}
