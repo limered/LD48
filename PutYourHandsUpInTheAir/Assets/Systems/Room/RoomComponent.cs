a51b6c2627bb2686a4612b10474e8dc3e4e74c75
Emil Wasilewski <emil.wasilewski@brickmakers.de>
2021-04-25 18:09:13 +0200

    add room system

---
+++ PutYourHandsUpInTheAir/Assets/Systems/Room/RoomComponent.cs
@@ -0,0 +1,16 @@
+﻿using SystemBase;
+using UniRx;
+using UnityEngine;
+
+namespace Systems.Room
+{
+    public class RoomComponent : GameComponent
+    {
+        public Vector2 SpawnInPosition;
+        public Vector2 GatherPosition;
+        public Vector2 SpawnOutPosition;
+        public float TimeInRoom = 30;
+        public float TimeLeftInRoom;
+        public FloatReactiveProperty RoomTimeProgress = new FloatReactiveProperty();
+    }
+}
---
+++ PutYourHandsUpInTheAir/Assets/Systems/Room/RoomSystem.cs
@@ -0,0 +1,30 @@
+﻿using SystemBase;
+using Systems.Tourist;
+using Systems.Tourist.States;
+using UniRx;
+using Utils.Plugins;
+
+namespace Systems.Room
+{
+    [GameSystem]
+    public class RoomSystem : GameSystem<RoomComponent, TouristBrainComponent>
+    {
+        public override void Register(RoomComponent component)
+        {
+            RegisterWaitable(component);
+        }
+
+        public override void Register(TouristBrainComponent component)
+        {
+            WaitOn<RoomComponent>()
+                .Subscribe(room => ResetTourist(component, room))
+                .AddToLifecycleOf(component);
+        }
+
+        private void ResetTourist(TouristBrainComponent component, RoomComponent room)
+        {
+            component.States.GoToState(new GoingIntoLevel());
+            component.transform.position = room.SpawnInPosition;
+        }
+    }
+}
