4a1f4772fd3c14164f4962b447544e7fd1a3c84b
Emil Wasilewski <emil.wasilewski@brickmakers.de>
2021-04-25 23:40:49 +0200

    add world

---
+++ PutYourHandsUpInTheAir/Assets/Systems/World/WorldComponent.cs
@@ -0,0 +1,12 @@
+﻿using SystemBase;
+using UnityEngine;
+
+namespace Systems.World
+{
+    public class WorldComponent : GameComponent
+    {
+        public GameObject[] EasyRooms;
+        public GameObject[] MediumRooms;
+        public GameObject[] HardRooms;
+    }
+}
---
+++ PutYourHandsUpInTheAir/Assets/Systems/World/WorldSystem.cs
@@ -0,0 +1,13 @@
+﻿using SystemBase;
+
+namespace Systems.World
+{
+    [GameSystem]
+    public class WorldSystem : GameSystem<WorldComponent>
+    {
+        public override void Register(WorldComponent component)
+        {
+            
+        }
+    }
+}
