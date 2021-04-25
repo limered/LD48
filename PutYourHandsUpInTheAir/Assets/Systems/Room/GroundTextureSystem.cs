c8165a9345edb05a1a86fed4645a30904cfcde9b
Emil Wasilewski <emil.wasilewski@brickmakers.de>
2021-04-25 22:06:45 +0200

    add ground system for ground texturing

---
+++ PutYourHandsUpInTheAir/Assets/Systems/Room/GroundTextureSystem.cs
@@ -0,0 +1,15 @@
+﻿using SystemBase;
+using UnityEngine;
+
+namespace Systems.Room
+{
+    [GameSystem]
+    public class GroundTextureSystem : GameSystem<GroundComponent>
+    {
+        public override void Register(GroundComponent component)
+        {
+            var renderer = component.GetComponent<SpriteRenderer>();
+            renderer.sprite = component.GroundTextures[(int) (Random.value * component.GroundTextures.Length)];
+        }
+    }
+}
