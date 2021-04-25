740023f316f6a6dd4b905e11bbaf3c822e9f2ce4
HelenBohleber <helen.bohleber@brickmakers.de>
2021-04-25 19:36:16 +0200

    trigger awakening animation for tiger

---
+++ PutYourHandsUpInTheAir/Assets/Systems/Distractions/TigerAnimationSystem.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using SystemBase;
+using Systems.DistractionControl;
+using Systems.Tourist;
+using Systems.Tourist.States;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+using Utils.Plugins;
+
+[GameSystem]
+public class TigerAnimationSystem : GameSystem<DistractionComponent>
+{
+    public override void Register(DistractionComponent component)
+    {
+        component.InteractionCollider
+            .OnTriggerEnterAsObservable()
+            .Subscribe(collider => StartWakeUpAnimation(collider, component))
+            .AddToLifecycleOf(component);
+    }
+
+    private void StartWakeUpAnimation(Collider collider, DistractionComponent component)
+    {
+        var collidingObject = collider.gameObject;
+        if (collidingObject.CompareTag("tourist"))
+        {
+            var animator = component.GetComponent<Animator>();
+            animator.Play("TigerWakingUp_Head");
+            animator.Play("TigerWakingUp_Tail");
+
+            collidingObject.GetComponent<TouristBrainComponent>().States.CurrentState
+            .Subscribe(state =>
+            {
+                HandleAnimationForState(component, collider, state);
+            })
+            .AddToLifecycleOf(component);
+        }
+    }
+
+    private void HandleAnimationForState(DistractionComponent component, Collider collider, SystemBase.StateMachineBase.BaseState<TouristBrainComponent> state)
+    {
+        var collidingObject = collider.gameObject;
+        if (collidingObject.CompareTag("tourist"))
+        {
+            var animator = component.GetComponent<Animator>();
+            if (state is Dead)
+            {
+                animator.Play("TigerFallingAsleep_Head");
+                //animator.Play("TigerKill");
+            }
+            else if (state is GoingBackToIdle)
+            {
+                animator.Play("TigerFallingAsleep_Head");
+            }
+        }
+    }
+}
