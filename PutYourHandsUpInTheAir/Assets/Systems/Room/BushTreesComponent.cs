a27fe378e657af5f4525a37255ab4821600c0402
HelenBohleber <helen.bohleber@brickmakers.de>
2021-04-26 22:46:55 +0200

    remove random generated bushes

--- PutYourHandsUpInTheAir/Assets/Systems/Room/BushTreesComponent.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using SystemBase;
-using UnityEngine;
-
-public class BushTreesComponent : GameComponent
-{
-    public GameObject[] BushTreesPrefabs;
-}
