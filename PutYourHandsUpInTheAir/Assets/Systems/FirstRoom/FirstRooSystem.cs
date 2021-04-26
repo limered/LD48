07f25268a421b08c98bfc1496520301480b3d791
Emil Wasilewski <emil.wasilewski@brickmakers.de>
2021-04-27 01:32:48 +0200

    add svens room

---
+++ PutYourHandsUpInTheAir/Assets/Systems/FirstRoom/FirstRooSystem.cs
@@ -0,0 +1,33 @@
+﻿using SystemBase;
+using Systems.Distractions;
+using UniRx;
+using Utils.Plugins;
+
+namespace Systems.FirstRoom
+{
+    public class FirstRooSystem : GameSystem<FirstRoomComponent, TigerDistractionTouristComponent>
+    {
+        private readonly ReactiveProperty<FirstRoomComponent> _firstRoomComponent = new ReactiveProperty<FirstRoomComponent>();
+
+        public override void Register(FirstRoomComponent component)
+        {
+            _firstRoomComponent.Value = component;
+        }
+
+        public override void Register(TigerDistractionTouristComponent component)
+        {
+            _firstRoomComponent.WhereNotNull()
+                .Subscribe(first => TigerDistractionTriggered(first, component))
+                .AddToLifecycleOf(component);
+        }
+
+        private void TigerDistractionTriggered(FirstRoomComponent firstRoom, TigerDistractionTouristComponent component)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public class FirstRoomComponent : GameComponent
+    {
+    }
+}
