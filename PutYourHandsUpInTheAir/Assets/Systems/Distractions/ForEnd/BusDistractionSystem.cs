d6b4906eedd476426d45ca927481cce35c525fea
Emil Wasilewski <emil.wasilewski@brickmakers.de>
2021-04-26 20:59:05 +0200

    add namespaces in bus and money system

--- PutYourHandsUpInTheAir/Assets/Systems/Distractions/ForEnd/BusDistractionSystem.cs
+++ PutYourHandsUpInTheAir/Assets/Systems/Distractions/ForEnd/BusDistractionSystem.cs
@@ -1,92 +1,95 @@
-using UnityEngine;
+using System.Linq;
 using SystemBase;
+using Systems.DistractionControl;
+using Systems.Movement;
 using Systems.Tourist;
 using Systems.Tourist.States;
 using UniRx;
 using UniRx.Triggers;
+using UnityEngine;
 using Utils.Plugins;
-using Systems.Movement;
-using System.Linq;
-using Systems.DistractionControl;
 
-[GameSystem]
-public class BusDistractionSystem : GameSystem<BusDistractionTouristComponent, BusComponent>
+namespace Systems.Distractions.ForEnd
 {
-    public override void Register(BusComponent bus)
+    [GameSystem]
+    public class BusDistractionSystem : GameSystem<BusDistractionTouristComponent, BusComponent>
     {
-        SystemUpdate()
-            .Where(_ => !bus.enteredScene)
-            .Subscribe(_ => {
-                var tourists = GameObject.FindGameObjectsWithTag("tourist");
-                if(!tourists.Any())
-                {
-                    return;
-                }
-
-                var allPaid = tourists.All(t => {
-                    var touristComp = t.GetComponent<TouristBrainComponent>();
-                    return touristComp && touristComp.HasPaid;
-                }
-                );
-                if (allPaid)
-                {
-                    bus.GetComponentInParent<DistractionComponent>().enabled = true;
-                    bus.enteredScene = true;
-                    var movementComp = bus.GetComponent<MovementComponent>();
-                    movementComp.Direction.Value = bus.StartPosititon - (Vector2)bus.transform.position;
-                }
-            })
-            .AddTo(bus);
-
-        SystemUpdate()
-            .Where(_ => bus.enteredScene && !bus.leftScene)
-            .Subscribe(_ =>
-            {
-                if(bus.transform.position.x > bus.StartPosititon.x)
-                {
-                    return;
-                }
-                bus.leftScene = true;
-                bus.transform.position = bus.StartPosititon;
-                var movementComp = bus.GetComponent<MovementComponent>();
-                movementComp.Direction.Value = Vector2.zero;
-            });
-    }
-
-    public override void Register(BusDistractionTouristComponent component)
-    {
-        var touristBrain = component.GetComponent<TouristBrainComponent>();
-        
-        touristBrain.States.GoToState(new GoingToAttraction(component.InteractionPosition.position));
-        touristBrain.States.CurrentState
-            .Where(state => state is Interacting)
-            .Subscribe(_ => StartInteracting(component, touristBrain))
-            .AddToLifecycleOf(component);
-    }
-
-    private static void StartInteracting(BusDistractionTouristComponent component, TouristBrainComponent touristBrain)
-    {
-        component.UpdateAsObservable()
-            .Where(_ => component)
-            .Subscribe(_ => DriveAway(component, touristBrain))
-            .AddToLifecycleOf(component);
-    }
-
-    private static void DriveAway(BusDistractionTouristComponent component, TouristBrainComponent touristBrain)
-    {
-        component.LastDistractionProgressTime -= Time.deltaTime;
-
-        var bus = GameObject.FindGameObjectWithTag("bus");
-        var busComponent = bus.GetComponent<BusComponent>();
-        var movementComponent = bus.GetComponent<MovementComponent>();
-        touristBrain.transform.parent = bus.transform;
-
-        if (component.LastDistractionProgressTime <= 0)
+        public override void Register(BusComponent bus)
         {
-            component.DistractionProgress.Value = 1;
-            movementComponent.Direction.Value = busComponent.EndPosititon - (Vector2) component.transform.position;
+            SystemUpdate()
+                .Where(_ => !bus.enteredScene)
+                .Subscribe(_ => {
+                    var tourists = GameObject.FindGameObjectsWithTag("tourist");
+                    if(!tourists.Any())
+                    {
+                        return;
+                    }
+
+                    var allPaid = tourists.All(t => {
+                            var touristComp = t.GetComponent<TouristBrainComponent>();
+                            return touristComp && touristComp.HasPaid;
+                        }
+                    );
+                    if (allPaid)
+                    {
+                        bus.GetComponentInParent<DistractionComponent>().enabled = true;
+                        bus.enteredScene = true;
+                        var movementComp = bus.GetComponent<MovementComponent>();
+                        movementComp.Direction.Value = bus.StartPosititon - (Vector2)bus.transform.position;
+                    }
+                })
+                .AddTo(bus);
+
+            SystemUpdate()
+                .Where(_ => bus.enteredScene && !bus.leftScene)
+                .Subscribe(_ =>
+                {
+                    if(bus.transform.position.x > bus.StartPosititon.x)
+                    {
+                        return;
+                    }
+                    bus.leftScene = true;
+                    bus.transform.position = bus.StartPosititon;
+                    var movementComp = bus.GetComponent<MovementComponent>();
+                    movementComp.Direction.Value = Vector2.zero;
+                });
         }
 
-        component.DistractionProgress.Value = 1 - component.LastDistractionProgressTime / component.MaxProgressTime;
+        public override void Register(BusDistractionTouristComponent component)
+        {
+            var touristBrain = component.GetComponent<TouristBrainComponent>();
+        
+            touristBrain.States.GoToState(new GoingToAttraction(component.InteractionPosition.position));
+            touristBrain.States.CurrentState
+                .Where(state => state is Interacting)
+                .Subscribe(_ => StartInteracting(component, touristBrain))
+                .AddToLifecycleOf(component);
+        }
+
+        private static void StartInteracting(BusDistractionTouristComponent component, TouristBrainComponent touristBrain)
+        {
+            component.UpdateAsObservable()
+                .Where(_ => component)
+                .Subscribe(_ => DriveAway(component, touristBrain))
+                .AddToLifecycleOf(component);
+        }
+
+        private static void DriveAway(BusDistractionTouristComponent component, TouristBrainComponent touristBrain)
+        {
+            component.LastDistractionProgressTime -= Time.deltaTime;
+
+            var bus = GameObject.FindGameObjectWithTag("bus");
+            var busComponent = bus.GetComponent<BusComponent>();
+            var movementComponent = bus.GetComponent<MovementComponent>();
+            touristBrain.transform.parent = bus.transform;
+
+            if (component.LastDistractionProgressTime <= 0)
+            {
+                component.DistractionProgress.Value = 1;
+                movementComponent.Direction.Value = busComponent.EndPosititon - (Vector2) component.transform.position;
+            }
+
+            component.DistractionProgress.Value = 1 - component.LastDistractionProgressTime / component.MaxProgressTime;
+        }
     }
 }
--- PutYourHandsUpInTheAir/Assets/Systems/Distractions/ForEnd/MoneyDistractionSystem.cs
+++ PutYourHandsUpInTheAir/Assets/Systems/Distractions/ForEnd/MoneyDistractionSystem.cs
@@ -8,44 +8,47 @@ using UnityEngine;
 using Utils.Plugins;
 using Object = UnityEngine.Object;
 
-[GameSystem]
-public class MoneyDistractionSystem : GameSystem<MoneyDistractionTouristComponent, PlayerComponent>
+namespace Systems.Distractions.ForEnd
 {
-    public override void Register(MoneyDistractionTouristComponent component)
+    [GameSystem]
+    public class MoneyDistractionSystem : GameSystem<MoneyDistractionTouristComponent, PlayerComponent>
     {
-        var touristBrain = component.GetComponent<TouristBrainComponent>();
-        touristBrain.States.GoToState(new GoingToAttraction(touristBrain.transform.position));
-        touristBrain.States.GoToState(new Interacting());
-        component.CreatedFrom.HasFired = component.CreatedFrom.FireOnce;
+        public override void Register(MoneyDistractionTouristComponent component)
+        {
+            var touristBrain = component.GetComponent<TouristBrainComponent>();
+            touristBrain.States.GoToState(new GoingToAttraction(touristBrain.transform.position));
+            touristBrain.States.GoToState(new Interacting());
+            component.CreatedFrom.HasFired = component.CreatedFrom.FireOnce;
 
-        component.LastDistractionProgressTime = component.CreatedFrom.DistractionInteractionDuration;
+            component.LastDistractionProgressTime = component.CreatedFrom.DistractionInteractionDuration;
 
-        WaitOn<PlayerComponent>()
-            .Subscribe(player => StartPlayerCollisionTracking(component, player))
-            .AddToLifecycleOf(component);
-    }
+            WaitOn<PlayerComponent>()
+                .Subscribe(player => StartPlayerCollisionTracking(component, player))
+                .AddToLifecycleOf(component);
+        }
 
-    public override void Register(PlayerComponent component)
-    {
-        RegisterWaitable(component);
-    }
+        public override void Register(PlayerComponent component)
+        {
+            RegisterWaitable(component);
+        }
 
-    private void StartPlayerCollisionTracking(
-        MoneyDistractionTouristComponent component,
-        PlayerComponent player)
-    {
-        player.OnTriggerEnterAsObservable()
-            .Subscribe(CollideWithPlayer)
-            .AddToLifecycleOf(component);
-    }
+        private void StartPlayerCollisionTracking(
+            MoneyDistractionTouristComponent component,
+            PlayerComponent player)
+        {
+            player.OnTriggerEnterAsObservable()
+                .Subscribe(CollideWithPlayer)
+                .AddToLifecycleOf(component);
+        }
 
-    private void CollideWithPlayer(Collider coll)
-    {
-        Object.Destroy(coll.gameObject.GetComponent<MoneyDistractionTouristComponent>());
-        var tourist = coll.gameObject.GetComponent<TouristBrainComponent>();
-        if (!tourist) return;
+        private void CollideWithPlayer(Collider coll)
+        {
+            Object.Destroy(coll.gameObject.GetComponent<MoneyDistractionTouristComponent>());
+            var tourist = coll.gameObject.GetComponent<TouristBrainComponent>();
+            if (!tourist) return;
 
-        tourist.HasPaid = true;
-        tourist.States.GoToState(new GoingBackToIdle(tourist.transform.position));
+            tourist.HasPaid = true;
+            tourist.States.GoToState(new GoingBackToIdle(tourist.transform.position));
+        }
     }
 }
