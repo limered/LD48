ac644021cc590629974d853a158ecb766a4dd296
HelenBohleber <helen.bohleber@brickmakers.de>
2021-05-17 18:57:25 +0200

    hide dying person message

--- PutYourHandsUpInTheAir/Assets/Systems/GameMessages/UISystem.cs
+++ PutYourHandsUpInTheAir/Assets/Systems/GameMessages/UISystem.cs
@@ -1,27 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using SystemBase;
 using Systems.Tourist;
 using UniRx;
-using UnityEngine;
-using UnityEngine.UI;
+using UniRx.Triggers;
 
 [GameSystem]
 public class UISystem : GameSystem<UIComponent>
 {
+    private float sec = 80f;
 
     public override void Register(UIComponent component)
     {
         MessageBroker.Default.Receive<ShowDeadPersonAction>()
             .Subscribe(msg =>
             {
-                component.Message.gameObject.SetActive(true);
-
+                ResetTime();
                 var text = component.Message.Text;
                 var face = component.Message.Image;
 
                 text.text = msg.TouristName + " died.";
                 var faces = component.GetComponentInParent<TouristConfigComponent>().topParts;
                 face.sprite = faces[msg.TouristFaceIndex];
+
+                component.Message.gameObject.SetActive(true);
+
             })
             .AddTo(component);
+
+        component.UpdateAsObservable().Subscribe(_ =>
+        {
+            if(sec > 0)
+            {
+                sec--;
+            } else
+            {
+                component.Message.gameObject.SetActive(false);
+                ResetTime();
+            }
+        });
+    }
+
+    private void ResetTime()
+    {
+        sec = 80f;
     }
 }
