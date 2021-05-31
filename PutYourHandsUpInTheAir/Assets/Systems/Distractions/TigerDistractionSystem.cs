2da932acafa448975c4f5a0e68e1353f9b19455e
Emil Wasilewski <emil.wasilewski@brickmakers.de>
2021-06-01 01:01:20 +0200

    add death of tourist in tiger system

--- PutYourHandsUpInTheAir/Assets/Systems/Distractions/TigerDistractionSystem.cs
+++ PutYourHandsUpInTheAir/Assets/Systems/Distractions/TigerDistractionSystem.cs
@@ -1,10 +1,9 @@
-﻿using SystemBase;
+﻿using System;
+using SystemBase;
 using Systems.DistractionControl;
-using Systems.Player;
 using Systems.Tourist;
 using Systems.Tourist.States;
 using UniRx;
-using UniRx.Triggers;
 using UnityEngine;
 using Utils.Plugins;
 using Object = UnityEngine.Object;
@@ -12,17 +11,38 @@ using Object = UnityEngine.Object;
 namespace Systems.Distractions
 {
     [GameSystem(typeof(DistractionControlSystem))]
-    public class TigerDistractionSystem : GameSystem<TigerDistractionTouristComponent, PlayerComponent>
+    public class TigerDistractionSystem : GameSystem<TigerDistractionTouristComponent>
     {
         public override void Register(TigerDistractionTouristComponent component)
         {
             var touristBrain = component.GetComponent<TouristBrainComponent>();
             touristBrain.States.GoToState(new GoingToAttraction(component.InteractionPosition.position));
+
+            SystemUpdate(touristBrain)
+                .Where(tourist => tourist.States.CurrentState.Value is Interacting)
+                .Subscribe(InteractWithTiger(component))
+                .AddToLifecycleOf(component);
         }
 
-        public override void Register(PlayerComponent component)
+        private static Action<TouristBrainComponent> InteractWithTiger(TigerDistractionTouristComponent component)
         {
-            RegisterWaitable(component);
+            return tourist =>
+            {
+                if (!component) return;
+
+                component.LastDistractionProgressTime -= Time.deltaTime;
+
+                if (component.LastDistractionProgressTime <= 0)
+                {
+                    component.DistractionProgress.Value = 1;
+                    component.GetComponent<TouristBrainComponent>()
+                        .States
+                        .GoToState(new Dead(component));
+                    Object.Destroy(component);
+                }
+
+                component.DistractionProgress.Value = 1 - component.LastDistractionProgressTime / component.MaxProgressTime;
+            };
         }
     }
 }
