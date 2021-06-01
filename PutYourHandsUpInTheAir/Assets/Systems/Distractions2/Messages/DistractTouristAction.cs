20839daf83b64c608e80a49fca84ae05af5b4060
Emil Wasilewski <emil.wasilewski@brickmakers.de>
2021-06-01 23:35:21 +0200

    add tourist to distract tourist action

--- PutYourHandsUpInTheAir/Assets/Systems/Distractions2/Messages/DistractTouristAction.cs
+++ PutYourHandsUpInTheAir/Assets/Systems/Distractions2/Messages/DistractTouristAction.cs
@@ -1,15 +1,19 @@
 ﻿using Systems.DistractionControl;
+using Systems.Tourist;
 
 namespace Systems.Distractions2.Messages
 {
     public class DistractTouristAction
     {
-        public DistractTouristAction(DistractionOriginComponent distractionOriginOrigin)
+        public DistractTouristAction(DistractionOriginComponent distractionOriginOrigin, 
+            TouristBrainComponent tourist)
         {
             DistractionOriginOrigin = distractionOriginOrigin;
+            Tourist = tourist;
         }
 
         private DistractionOriginComponent DistractionOriginOrigin { get; }
+        public TouristBrainComponent Tourist { get; }
 
         public T GetOrigin<T>() where T : DistractionOriginComponent, new()
         {
