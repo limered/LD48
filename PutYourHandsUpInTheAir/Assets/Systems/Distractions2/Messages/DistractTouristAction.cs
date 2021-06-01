349ce44035cf36cc00c0cdac1225795f49b284c3
Emil Wasilewski <emil.wasilewski@brickmakers.de>
2021-06-01 20:28:40 +0200

    add start distraction message

---
+++ PutYourHandsUpInTheAir/Assets/Systems/Distractions2/Messages/DistractTouristAction.cs
@@ -0,0 +1,19 @@
+﻿using Systems.DistractionControl;
+
+namespace Systems.Distractions2.Messages
+{
+    public class DistractTouristAction
+    {
+        public DistractTouristAction(DistractionOriginComponent distractionOriginOrigin)
+        {
+            DistractionOriginOrigin = distractionOriginOrigin;
+        }
+
+        private DistractionOriginComponent DistractionOriginOrigin { get; }
+
+        public T GetOrigin<T>() where T : DistractionOriginComponent, new()
+        {
+            return (T)DistractionOriginOrigin;
+        }
+    }
+}
