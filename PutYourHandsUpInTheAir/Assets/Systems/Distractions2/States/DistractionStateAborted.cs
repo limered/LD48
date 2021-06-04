5422eee63844f970c4371d6093cd3e9d16db458f
Emil Wasilewski <emil.wasilewski@brickmakers.de>
2021-06-04 02:53:27 +0200

    add states for distraction origins

---
+++ PutYourHandsUpInTheAir/Assets/Systems/Distractions2/States/DistractionStateAborted.cs
@@ -0,0 +1,12 @@
+﻿using SystemBase.StateMachineBase;
+
+namespace Systems.Distractions2.States
+{
+    public class DistractionStateAborted : BaseState<DistractionOriginComponent>
+    {
+        public override void Enter(StateContext<DistractionOriginComponent> context)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ PutYourHandsUpInTheAir/Assets/Systems/Distractions2/States/DistractionStateWalkingIn.cs
@@ -0,0 +1,12 @@
+﻿using SystemBase.StateMachineBase;
+
+namespace Systems.Distractions2.States
+{
+    public class DistractionStateWalkingIn : BaseState<DistractionOriginComponent>
+    {
+        public override void Enter(StateContext<DistractionOriginComponent> context)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
