6db4563b9c2e685e7dcb5627d50260c184168351
Emil Wasilewski <emil.wasilewski@brickmakers.de>
2021-06-04 02:55:19 +0200

    tiger distraction can be cancelled now

--- PutYourHandsUpInTheAir/Assets/Systems/Distractions2/DistractionStrategies/TigerDistraction.cs
+++ PutYourHandsUpInTheAir/Assets/Systems/Distractions2/DistractionStrategies/TigerDistraction.cs
@@ -1,35 +1,50 @@
-﻿using SystemBase.StateMachineBase;
+﻿using System;
+using Systems.Distractions2.States;
 using Systems.Tourist;
 using Systems.Tourist.States;
+using UniRx;
 using UnityEngine;
 
 namespace Systems.Distractions2.DistractionStrategies
 {
     public class TigerDistraction : IDistraction
     {
-        private float MaxDistractionDuration = 3.0f;
-        private float LastDistractionProgressTime = 3.0f;
+        private float _maxDistractionDuration = 3.0f;
+        private float _lastDistractionProgressTime = 3.0f;
+        private bool _cancelledByPlayer;
+        private IDisposable _abortedDisposable;
 
         public void Init(DistractableComponent distractable)
         {
-            MaxDistractionDuration = distractable.Origin.DistractionInteractionDuration;
-            LastDistractionProgressTime = MaxDistractionDuration;
+            _maxDistractionDuration = distractable.Origin.DistractionInteractionDuration;
+            _lastDistractionProgressTime = _maxDistractionDuration;
+
+            _abortedDisposable = distractable.Origin.StateContext.CurrentState
+                .Where(state => state is DistractionStateAborted)
+                .Subscribe(_ => _cancelledByPlayer = true);
         }
 
         public DistractionOutcome Update(DistractableComponent distractable)
         {
+            if (_cancelledByPlayer)
+            {
+                _abortedDisposable.Dispose();
+                return DistractionOutcome.Alive;
+            }
+
             var touristBrain = distractable.GetComponent<TouristBrainComponent>();
 
             if (touristBrain.StateContext.CurrentState.Value is Interacting)
             {
-                LastDistractionProgressTime -= Time.fixedDeltaTime;
-                if (LastDistractionProgressTime <= 0)
+                _lastDistractionProgressTime -= Time.fixedDeltaTime;
+                if (_lastDistractionProgressTime <= 0)
                 {
+                    _abortedDisposable.Dispose();
                     return DistractionOutcome.Dead;
                 }
             }
 
-            distractable.DistractionProgress.Value = 1 - LastDistractionProgressTime / MaxDistractionDuration;
+            distractable.DistractionProgress.Value = 1 - _lastDistractionProgressTime / _maxDistractionDuration;
             return DistractionOutcome.Waiting;
         }
     }
