002c4a2ddccc5447fa0a26a3b5ff4efbb8ff3b2e
Emil Wasilewski <emil.wasilewski@brickmakers.de>
2021-08-09 20:52:38 +0200

    add paper mario style rotation

---
+++ PutYourHandsUpInTheAir/Assets/Systems/Movement/PaperMarioStyleAnimationComponent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using SystemBase;
+using UnityEngine;
+
+namespace Systems.Movement
+{
+    [RequireComponent(typeof(TwoDeeMovementComponent))]
+    public class PaperMarioStyleAnimationComponent : GameComponent
+    {
+        public float AnimationDelay;
+        public IDisposable AnimationDisposable { get; set; }
+
+        public IEnumerator Turn(Vector3 from, Vector3 to)
+        {
+            for (var ft = -.1f; ft <= 1.1f; ft += 0.1f)
+            {
+                transform.localScale = Vector3.Lerp(from, to, ft);
+                yield return new WaitForSeconds(.1f);
+            }
+        }
+    }
+}
---
+++ PutYourHandsUpInTheAir/Assets/Systems/Movement/PaperMarioStyleAnimationSystem.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using SystemBase;
+using UniRx;
+using UnityEngine;
+
+namespace Systems.Movement
+{
+    [GameSystem]
+    public class PaperMarioStyleAnimationSystem : GameSystem<PaperMarioStyleAnimationComponent>
+    {
+        public override void Register(PaperMarioStyleAnimationComponent component)
+        {
+            var twoDeeComp = component.GetComponent<TwoDeeMovementComponent>();
+            twoDeeComp.Direction
+                .Pairwise()
+                .Where(DirectionChanged)
+                .Throttle(TimeSpan.FromMilliseconds(component.AnimationDelay))
+                .Select(pair => (pair.Current, component))
+                .Subscribe(ChangeDirection)
+                .AddTo(component);
+        }
+
+        private void ChangeDirection((Vector2 Current, PaperMarioStyleAnimationComponent component) data)
+        {
+            var (Current, component) = data;
+            if (Current.x < 0)
+            {
+                component.AnimationDisposable?.Dispose();
+                component.AnimationDisposable = Observable
+                    .FromMicroCoroutine(_ => component.Turn(component.transform.localScale, new Vector3(-1, 1, 1)))
+                    .Subscribe();
+            }
+            else
+            {
+                component.AnimationDisposable?.Dispose();
+                component.AnimationDisposable = Observable
+                    .FromMicroCoroutine(_ => component.Turn(component.transform.localScale, new Vector3(1, 1, 1)))
+                    .Subscribe();
+            }
+        }
+
+        private bool DirectionChanged(Pair<Vector2> pair)
+        {
+            return pair.Current.x < 0 && pair.Previous.x >= 0 || pair.Current.x > 0 && pair.Previous.x <= 0;
+        }
+    }
+}
