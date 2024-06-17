e4a1e8d9170e18cb573e3f05284dab2bc6d5385f
limered <me@limered.de>
2024-06-18 00:08:12 +0200

    send player to click position

--- PutYourHandsDownOnTheGround/Hero/HeroNode.cs
+++ PutYourHandsDownOnTheGround/Hero/HeroNode.cs
@@ -5,17 +5,27 @@ namespace IsThisATiger2.Empty.Hero;
 
 public partial class HeroNode : Node2D
 {
-	private MovementNode2D _movement;
-	private Vector2 _targetVector;
-	private Node2D _targetedTourist;
-	
-	public override void _Ready()
-	{
-	}
+    private MovementNode2D _movement;
+    private Node2D _targetedTourist;
+    private Vector2 _targetVector;
 
-	public override void _Process(double delta)
-	{
-		var targetVec = _targetedTourist?.Position ?? _targetVector;
-		_movement.Direction = (targetVec - Position).Normalized();
-	}
+    public override void _Ready()
+    {
+        _movement = GetNode<MovementNode2D>("Movement");
+    }
+
+    public override void _Process(double delta)
+    {
+        var targetVec = _targetedTourist?.Position ?? _targetVector;
+        if (targetVec.DistanceTo(Position) < 0.001) _movement.Direction = Vector2.Zero;
+        _movement.Direction = Position.DirectionTo(targetVec);
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+        if (@event is InputEventMouseButton eventMouseButton)
+        {
+            _targetVector = eventMouseButton.Position - GetViewport().GetVisibleRect().Size / 2;
+        }
+    }
 }
