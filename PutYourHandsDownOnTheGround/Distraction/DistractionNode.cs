47ffb3aa696d1a6ee882068a354e8fca1769f603
limered <me@limered.de>
2024-06-23 02:11:43 +0200

    add parameters to Butterfly.tscn

--- PutYourHandsDownOnTheGround/Distraction/DistractionNode.cs
+++ PutYourHandsDownOnTheGround/Distraction/DistractionNode.cs
@@ -5,19 +5,19 @@ namespace IsThisATiger2.Empty.Distraction;
 public partial class DistractionNode : Node2D
 {
 	[Export] public float DistractionDuration;
+	[Export] public string IdleAnimation;
+	[Export] public string KillAnimation;
+	[Export] public Texture2D WaitingTimeBubble;
 	public PackedScene Bubble => GD.Load<PackedScene>("res://Scenes/ThinkingBubble.tscn");
 
 	public override void _Ready()
 	{
 		GetNode<DistractionCollection>("/root/DistractionCollection").Add(this);
 		GetNode<AnimationPlayer>("Sprite2D/AnimationPlayer")
-			.Play("flutter");
+			.Play(IdleAnimation);
 	}
 
 	public override void _Process(double delta)
 	{
-		// Instantiate Distraction to Tourist Connection Scene on Tourist
-		// Tick Distraction To Tourist Connection
-		// If Time is over, execute the action  
 	}
 }
