4cb5ac2e064bd998ad49535af20d5a1dd7b74257
limered <me@limered.de>
2024-06-26 00:50:16 +0200

    animate turn

--- PutYourHandsDownOnTheGround/Physics/JumpyRunningAnimation.cs
+++ PutYourHandsDownOnTheGround/Physics/JumpyRunningAnimation.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Threading.Tasks;
 using Godot;
 
 namespace IsThisATiger2.Empty.Physics;
@@ -6,6 +8,7 @@ public partial class JumpyRunningAnimation : Node2D
 {
     private double _time;
     private Vector2 _wobbleAxis = Vector2.Up;
+    private Vector2 _lastVelocity;
     
     [Export] public MovementNode2D Movement;
     [Export] public float WobbleFactor = 0.1f;
@@ -18,5 +21,17 @@ public partial class JumpyRunningAnimation : Node2D
         var a = Mathf.Sin(_time / WobbleInterval) * speed * WobbleFactor;
         a = Mathf.Abs(a) + a;
         Position = _wobbleAxis * (float)a;
+
+        if (_lastVelocity.X <= 0 && Movement.Velocity.X > 0)
+        {
+            var tween = GetTree().CreateTween();
+            tween.TweenProperty(this, "scale", new Vector2(1, 1), 0.2f);
+        }
+        else if (_lastVelocity.X >= 0 && Movement.Velocity.X < 0)
+        {
+            var tween = GetTree().CreateTween();
+            tween.TweenProperty(this, "scale", new Vector2(-1, 1), 0.2f);
+        }
+        _lastVelocity = Movement.Velocity;
     }
 }
