13a3fcb6461117b4c5c41e265069db0e4718ef55
limered <me@limered.de>
2024-08-17 00:02:26 +0200

    add animated bush

---
+++ PutYourHandsDownOnTheGround/Plants/BushNode.cs
@@ -0,0 +1,43 @@
+using Godot;
+
+namespace IsThisATiger2.Empty.Plants;
+
+public partial class BushNode : Node2D
+{
+	[Export] public bool FlipH;
+	[Export] public Texture2D[] Bushes;
+	[Export] public int BushSpriteId;
+	
+	public override void _Ready()
+	{
+		var sprite = GetNode<Sprite2D>("Sprite2D");
+		sprite.Texture = Bushes[BushSpriteId];
+		sprite.FlipH = FlipH;
+
+		var area = GetNode<Area2D>("Area2D");
+		area.AreaEntered += OnAreaEntered;
+		area.AreaExited += OnAreaEntered;
+
+		area.MouseEntered += AreaOnMouseEntered;
+	}
+
+	private void AreaOnMouseEntered()
+	{
+		Animate();
+	}
+
+	private void OnAreaEntered(Area2D area)
+	{
+		Animate();
+	}
+
+	private void Animate()
+	{
+		var rnd = GD.RandRange(-0.3d, 0.3d);
+		var tween = GetTree().CreateTween();
+		tween.TweenProperty(this, "skew", rnd, 0.05f);
+		tween.TweenProperty(this, "skew", 0f, 0.05f);
+		tween.TweenProperty(this, "skew", -rnd, 0.05f);
+		tween.TweenProperty(this, "skew", 0f, 0.05f);
+	}
+}
